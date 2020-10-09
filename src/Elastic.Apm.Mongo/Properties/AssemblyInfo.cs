4fa3f0cb3c546593c98b30361d635b08e6adb447
Vadim Hatsura <vadim.hatsura@gmail.com>
2020-10-09 11:43:19 +0300

    set assembly info attributes

--- src/Elastic.Apm.Mongo/Properties/AssemblyInfo.cs
+++ src/Elastic.Apm.Mongo/Properties/AssemblyInfo.cs
@@ -2,4 +2,10 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
+[assembly: AssemblyCompany("Vadim Hatsura")]
+[assembly: AssemblyCopyright("Copyright (c) 2019 - Present")]
+[assembly: AssemblyDescription("Elastic APM for MongoDB. This package contains auto instrumentation for MongoDb.")]
+[assembly: AssemblyProduct("Elastic.Apm.Mongo")]
+[assembly: AssemblyTitle("Elastic.Apm.Mongo")]
+
 [assembly: InternalsVisibleTo("Elastic.Apm.Mongo.Tests")]
